2e70a871ddf94ae1cb03184823768528735686b0
Pablo B <bautista863@yahoo.com>
2019-10-18 18:57:36 -0500

    Update JavaScripts.js

--- JavaScripts.js
+++ JavaScripts.js
@@ -3,3 +3,16 @@ function paymentTotal(ShoppingAmount, Tax, ShippingCharges) {
 	
 	return Total;
 }
+
+function validatePhone(phone){
+	var phoneNumber = document.getElementById("phone").value;
+
+	var phoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+
+	var phoneResult = phoneRGEX.test(phoneNumber);
+
+	if(phoneResult == false)
+	{
+		alert('Please enter a valid phone number');
+		return false;
+	}}
