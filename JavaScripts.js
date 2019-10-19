18cb627b03dd4e61cf005188d3c86c1222cc1393
Pablo B <bautista863@yahoo.com>
2019-10-18 21:31:13 -0500

    Update JavaScripts.js

--- JavaScripts.js
+++ JavaScripts.js
@@ -3,9 +3,20 @@ function paymentTotal(ShoppingAmount, Tax, ShippingCharges) {
 	
 	return Total;
 }
+function paymentTotalTax(ShoppingAmount) {
+	var TotalTax = ShoppingAmount * .08;
 
-function validatePhone(phone){
-	var phoneNumber = document.getElementById("phone").value;
+	return TotalTax;
+}
+function paymentShipping(ShoppingAmount) {
+	var  TotalShipping = ShoppingAmount * .03;
+
+	return TotalShipping;
+}
+
+
+function validatePhone(phoneNumber){
+	var phoneNumber = document.getElementById(phone).value;
 
 	var phoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
 
