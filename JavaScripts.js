ae017f70a194dfe31fe3d468183a0f68fc403e9d
Pablo B <bautista863@yahoo.com>
2019-10-18 23:02:49 -0500

    added email verifcation function

--- JavaScripts.js
+++ JavaScripts.js
@@ -33,3 +33,19 @@ function calculateShoppingCart(price, amount) {
 	
 	return TotalPrice;
 }
+
+function ValidateEmail(Email)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
