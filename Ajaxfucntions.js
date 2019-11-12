38dacdcc1344af904c27decb305dec614d2ff1c5
Pablo B <bautista863@yahoo.com>
2019-11-11 18:04:49 -0600

    Update Ajaxfucntions.js

--- Ajaxfucntions.js
+++ Ajaxfucntions.js
@@ -1,6 +1,8 @@
 
 
-// User info for address
+// User info for address//
+
+
 var userInfo = {
     name: "",
     addr: ["", ""],
@@ -62,38 +64,7 @@ function loadIframeContents(){
 
     // Sets up functions and listeners for pages
 
-    // SHOPPING CART
-    if(!submitButton){
 
-        submitButton = iframe.contentWindow.document.getElementById("cart-submit");
-        submitFunc = cartSubmit;
-
-        // Populate cart details when a new unit size or product is select
-        qtySelect = iframe.contentWindow.document.querySelector('[name="quantity"]');
-        if(qtySelect){
-            qtySelect.addEventListener('input', populateCart);
-            iframe.contentWindow.document.getElementById("add-button").addEventListener('click', addItem);
-        }
-
-        // Reset quanitity to 1 when changing products
-        partSelect = iframe.contentWindow.document.querySelector('[name="parts"]');
-        if(partSelect){
-            partSelect.addEventListener('change', function(){
-                qtySelect.value = 1;
-                populateCart();
-            });
-        }
-        if(HTMLTable){
-
-            if(iframe.contentWindow.document.getElementById("addTable")){ iframe.contentWindow.document.getElementById("addTable").replaceWith(HTMLTable);
-            }
-
-            if(iframe.contentWindow.document.getElementById("cart-total")){
-                iframe.contentWindow.document.getElementById("cart-total").value = "$" + cart.totalAmt.toFixed(2);
-            }
-        }
-
-    }
     // USER INFO
     if(!submitButton){
 
@@ -110,40 +81,6 @@ function loadIframeContents(){
             iframe.contentWindow.document.querySelector('[name="zip"]').value =  userInfo.zip;
         }
     }
-    // SHIPPING INFO
-    if(!submitButton){
-
-        submitButton = iframe.contentWindow.document.getElementById("ship-submit");
-        submitFunc = shipSubmit;
-        physicalAddr = iframe.contentWindow.document.getElementById("physical-address");
-
-        // Populate address info with physical address in user info if checkbox is checked
-        if(submitButton){
-            physicalAddr.addEventListener("change", usePhysicalAddr);
-        }
-    }
-    // CHECKOUT
-    if(!submitButton){
-
-        submitButton = iframe.contentWindow.document.getElementById("checkout-submit");
-        submitFunc = checkoutSubmit;
-
-        // Populate checkout values using values already calculated in shopping cart
-        if(submitButton){
-
-            if(HTMLTable){
-                iframe.contentWindow.document.getElementById("checkout-table-container").appendChild(HTMLTable);
-            }
-
-            iframe.contentWindow.document.getElementById("cart-amount").value = "$" + cart.cartAmt.toFixed(2);
-            iframe.contentWindow.document.getElementById("tax-amount").value = "$" + cart.taxAmt.toFixed(2);
-            iframe.contentWindow.document.getElementById("ship-amount").value = "$" + cart.shipAmt.toFixed(2);
-            iframe.contentWindow.document.getElementById("total-amount").value = "$" + cart.totalAmt.toFixed(2);
-
-
-
-        }
-    }
 
     // Grabs form element of iframe page and changes submit button function
     if(submitButton){
@@ -152,95 +89,12 @@ function loadIframeContents(){
     }
 }
 
-function populateCart() {
 
-    unitPrice = iframe.contentWindow.document.querySelector('#unit-price');
-    totalPrice = iframe.contentWindow.document.querySelector('#total-price');
-
-    qty = qtySelect.value;
-    qty = qty * 1.0;
-    // Check object for given name and then populate fields based on product info
-    for(var i=0; i<items.length; i++){
-        if(items[i].title == partSelect.value){
-            price = items[i].price.toFixed(2);
-            itemSelected = items[i].title;
-            totalPrice.value = "$" + (price * qty).toFixed(2);
-            unitPrice.value = "$" + price;
-            //tablePrice.value = "$" + price;
-
-            //itemSel.value = itemSelected;
-            break;
-        }
-        totalPrice.value = "";
-        unitPrice.value = "";
-    }
-
-
-}
-
-function populateSummary() {
-
-    totalPrice = iframe.contentWindow.document.querySelector('#total-price');
-    qty = qtySelect.value;
-    qty = qty * 1.0;
-    // Check object for given name and then populate fields based on product info
-    for(var i=0; i<items.length; i++){
-        if(items[i].title == partSelect.value){
-            price = items[i].price.toFixed(2);
-            totalPrice.value = "$" + (price * qty).toFixed(2);
-            unitPrice.value = "$" + price;
-            break;
-        }
-        totalPrice.value = "";
-        unitPrice.value = "";
-    }
-
-}
-
-
-function addItem(){
-    cart.calc(qty, price);
-    console.log(cart);
-
-    partSelect.value = "";
-    qtySelect.value = "1";
-
-    iframe.contentWindow.document.getElementById("cart-total").value = "$" + cart.totalAmt.toFixed(2);
-
-    row = iframe.contentWindow.document.createElement("tr");
-    iframe.contentWindow.document.getElementById("addTable").appendChild(row);
-
-    data = iframe.contentWindow.document.createElement("td");
-    dataText = iframe.contentWindow.document.createTextNode(itemSelected);
-    data.appendChild(dataText);
-    row.appendChild(data);
-
-
-    data = iframe.contentWindow.document.createElement("td");
-    dataText = iframe.contentWindow.document.createTextNode("$" + price);
-    data.appendChild(dataText);
-    row.appendChild(data);
-
-
-    data = iframe.contentWindow.document.createElement("td");
-    dataText = iframe.contentWindow.document.createTextNode(qty);
-    data.appendChild(dataText);
-    row.appendChild(data);
-
-
-    HTMLTable = iframe.contentWindow.document.getElementById("addTable").cloneNode(true);
-
-
-}
-
-function cartSubmit(){
-
-}
 
 function userInfoSubmit(){
 
     // Save user info in object
-    userInfo.name = iframe.contentWindow.document.querySelector('[name="fullname"]').value;
+    userInfo.name = iframe.contentWindow.document.querySelector('[name="FullName"]').value;
     userInfo.addr[0] = iframe.contentWindow.document.querySelector('[name="addr1"]').value;
     userInfo.addr[1] = iframe.contentWindow.document.querySelector('[name="addr2"]').value;
     userInfo.city = iframe.contentWindow.document.querySelector('[name="city"]').value;
@@ -301,7 +155,7 @@ function showStates(str) {
             document.getElementById("state").innerHTML = this.responseText;
         }
     };
-    xhttp.open("POST", "States.php?q="+str, true);
+    xhttp.open("POST", "getstates.php?q="+str, true);
     xhttp.send();
 }
 
