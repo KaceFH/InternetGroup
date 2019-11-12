bceac583269696183095d5050a7dc89a0eb92fe3
Pablo B <bautista863@yahoo.com>
2019-11-12 15:56:29 -0600

    Update Ajaxfucntions.js

--- Ajaxfucntions.js
+++ Ajaxfucntions.js
@@ -144,18 +144,33 @@ function row() {
     newCell2.innerHTML = "Sudo Placement";
 }
 
+
+
 // AJAX - get states from database
-function showStates(str) {
-    var xhttp;
-    if (str == "") {
-        document.getElementById("state").innerHTML = "";
-        return;
-    }xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() {
+
+function showStates() {
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("state").innerHTML = this.responseText;
+            var obj = eval(this.responseText);
+            var x = document.getElementById("stateDD");
+
+            for (i = 0; i < obj.length; i++) {
+                var option = document.createElement("option");
+                option.value = obj[i].stateCode;
+                option.text = obj[i].stateName;
+                x.add(option, x[i]);
+            }
         }
     };
-    xhttp.open("POST", "getstates.php?q="+str, true);
-    xhttp.send();
+    xmlhttp.open("GET","getstates.php",true);
+    xmlhttp.send();
+
 }
 
