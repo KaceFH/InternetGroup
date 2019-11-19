9485d2f6f61d7e94300f05112a672d9262de7566
Pablo B <bautista863@yahoo.com>
2019-11-19 13:12:22 -0600

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,35 +1,64 @@
+
 <html>
+
+
 
 <body>
 
+
+
 <?php
+
 $con = mysqli_connect("localhost","root","","CS3320");
+
 if (!$con)
+
 {
+
     die('Could not connect: ' . mysql_error());
+
 }
+
 else echo "connected!!!<br>";
 
-//mysqli_select_db($con);
-//  var_dump( $_POST );
-//  echo $_POST['fName'];
+
+
 $sql="INSERT INTO userinformation (fName, lName, address1, address2, city, state, zip, phone, email)
+
 VALUES
+
 ('$_POST[fName]','$_POST[lName]','$_POST[address1]','$_POST[address2]','$_POST[city]','$_POST[state]','$_POST[zip]','$_POST[phone]','$_POST[email]')";
-//('Test','Tester')";
+
+
+
 echo $sql;
-//execute the INSERT
+
+
+
 if (!mysqli_query($con,$sql))
+
 {
+
     die('Error: ' . mysqli_error($con));
+
 }
+
 else {
+
     echo "<br>1 record added";
+
 }
+
+
 
 mysqli_close($con)
+
 ?>
 
+
+
 </body>
+
+
 
 </html>
