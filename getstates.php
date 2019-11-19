d35b28340f84c4d55ee953f8f4f70075e1718294
Pablo B <bautista863@yahoo.com>
2019-11-18 18:19:35 -0600

    Update getstates.php

--- getstates.php
+++ getstates.php
@@ -1,45 +1,100 @@
+
 <?php
+
 $con = mysqli_connect("localhost","root","","CS3320");
+
 if (!$con)
+
 {
+
+
 
     die('Could not connect: ' . mysql_error());
 
+
+
 }
+
 $sql="select * from states";
+
 //where stateCode =
+
 //('$_GET[stateCode]')";
+
+
 
 //echo $sql;
 
+
+
 //execute the INSERT
+
 $result = mysqli_query($con,$sql);
-//$returnStates = "[";
+
+$returnStates = "AL ";
+
+
+
 
 
 while($row = mysqli_fetch_array($result)) {
-    /*$returnStates = $returnStates .
+    $returnStates = $returnStates . $row['stateCode'] ." ";
+/*
+    $returnStates = $returnStates .
+
         "{stateCode: \"" .
-        $row['stateCode'] .
-        "\"," . "stateName:
-        \"" .
-        $row['stateName'] . "\"}
-        ,";*/
-   // second attempt
 
-    $id = $row['stateID'];
-    $code = $row['stateCode'];
-    $name = $row['stateName'];
+        $row['stateCode'] ."\"}";
+        // \"," . "stateName:
 
+       // \"" .
+
+       // $row['stateName'] . "\"}
+
+        //,";
+*/
+
+    // second attempt
+
+
+
+    //$id = $row['stateID'];
+
+    //$code = $row['stateCode'];
+
+    //$name = $row['stateName'];
+
+
+/*
     $return_arr[] = array("id" => $id,
+
         "code" => $code,
+
         "name" => $name);
+*/
+
 
 }
+
+//str_split ( string $returnStates [, int $split_length = 2 ] ) : array;
+//$stateArray = array();
+//while($row = mysqli_fetch_array($result)) {
+
+//}
+//$arr2 = str_split($str, 3);
 //$returnStates = $returnStates ."]";
-//echo $returnStates;
-$test = "TESTTESTEST";
-echo $test;
+
+echo $returnStates;
+//$test = array();
+//$test = str_split($returnStates, 2);
+
+//echo $test;
+//foreach($test as $test) {
+//    echo $test, "<br>";
+//}
+
 //return $returnStates;
+
 mysqli_close($con);
+
 ?>
